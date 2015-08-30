d309b3e4aa333790e26f88453a93bd288c6f94ce
Peter Grobarcik <peter.grobarcik@nttdata.com>
2015-08-30 21:28:06 +0200

    Added top level CMakeLists.txt

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/grobap/wrk/wks-yubico/trihlav/src/trihlavSrv
+# Install script for directory: /home/grobap/wrk/wks-yubico/trihlav
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -32,20 +32,10 @@ if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
   set(CMAKE_INSTALL_SO_NO_EXE "0")
 endif()
 
-if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
-  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/trihlavsrv" AND
-     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/trihlavsrv")
-    file(RPATH_CHECK
-         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/trihlavsrv"
-         RPATH "")
-  endif()
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "/home/grobap/wrk/wks-yubico/trihlav/src/trihlavSrv/trihlavsrv")
-  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/trihlavsrv" AND
-     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/trihlavsrv")
-    if(CMAKE_INSTALL_DO_STRIP)
-      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/trihlavsrv")
-    endif()
-  endif()
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/home/grobap/wrk/wks-yubico/trihlav/src/cmake_install.cmake")
+
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -54,7 +44,7 @@ else()
   set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 endif()
 
-file(WRITE "/home/grobap/wrk/wks-yubico/trihlav/src/trihlavSrv/${CMAKE_INSTALL_MANIFEST}" "")
+file(WRITE "/home/grobap/wrk/wks-yubico/trihlav/${CMAKE_INSTALL_MANIFEST}" "")
 foreach(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  file(APPEND "/home/grobap/wrk/wks-yubico/trihlav/src/trihlavSrv/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+  file(APPEND "/home/grobap/wrk/wks-yubico/trihlav/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
 endforeach()
