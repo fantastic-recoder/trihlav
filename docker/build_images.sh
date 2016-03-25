ff06d89188c9261db77c015b45f0cd430d63636f
Peter Grobarcik <peter.grobarcik@nttdata.com>
2016-03-25 18:33:02 +0100

    Added first docker scripts and OpenSuSE tumbleweed image.

---
+++ docker/build_images.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+make -C ../../trihlav-build package
+rm -f trihlav-OpenSuSE-tumbleweed/trihlav-0.0.1-Linux.rpm
+cp ../../trihlav-build/trihlav-0.0.1-Linux.rpm trihlav-OpenSuSE-tumbleweed
+docker build -t opensuse/tumbleweed:v1 docker-containers-build/docker
+docker build -t gcr.io/trihlav-1/trihlav-1-node:v1 trihlav-OpenSuSE-tumbleweed
