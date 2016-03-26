ce30a14242abf415fbab6b49fd578a7a34f44665
Peter Grobarcik <peter.grobarcik@nttdata.com>
2016-03-26 21:18:38 +0100

    Fix module setup.

--- src/main/docker/build_images.sh
+++ src/main/docker/build_images.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -ex
-export TRH_VER=$(date +%Y%m%d)
-echo Pushing version $TRH_VER.
-make -C ../../trihlav-build package
+export TRH_VER=$(date +%Y%m%d.%H%M%S)
+export TRH_LOCK=/tmp/trihlav_lock.txt
+if [ -f $TRH_LOCK ]; then
+	echo Job is already running\!
+	source $TRH_LOCK
+fi
+set +x
+echo "export TRH_VER=$TRH_VER" > $TRH_LOCK
+echo "*******************************************"
+echo "* Pushing version $TRH_VER.               *"
+echo "*******************************************"
+make -C ../../../../trihlav-build package
 rm -f trihlav-OpenSuSE-tumbleweed/trihlav-0.0.1-Linux.rpm
-cp ../../trihlav-build/trihlav-0.0.1-Linux.rpm trihlav-OpenSuSE-tumbleweed
+cp ../../../../trihlav-build/trihlav-0.0.1-Linux.rpm trihlav-OpenSuSE-tumbleweed
 docker build -t opensuse/tumbleweed:v1 docker-containers-build/docker
 docker build -t gcr.io/trihlav-1/trihlav-1-node:$TRH_VER trihlav-OpenSuSE-tumbleweed
 set +e
@@ -13,3 +22,5 @@ kubectl delete deployment,service trihlav-1-node
 gcloud docker push gcr.io/trihlav-1/trihlav-1-node:$TRH_VER
 kubectl run trihlav-1-node --image=gcr.io/trihlav-1/trihlav-1-node:$TRH_VER --port=32899 --expose --service-overrides='{ "spec": { "type": "LoadBalancer" } }'
 kubectl get pods
+set -e
+rm -rf $TRH_LOCK
