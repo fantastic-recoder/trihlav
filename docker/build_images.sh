08e8adf93ccfdfed36b1481be48b76a58030ae9f
Peter Grobarcik <peter.grobarcik@nttdata.com>
2016-03-26 15:30:58 +0100

    Small dockerfile better deploy & push 2 gcl script.

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+set -ex
+export TRH_VER=$(date +%Y%m%d)
+echo Pushing version $TRH_VER.
 make -C ../../trihlav-build package
 rm -f trihlav-OpenSuSE-tumbleweed/trihlav-0.0.1-Linux.rpm
 cp ../../trihlav-build/trihlav-0.0.1-Linux.rpm trihlav-OpenSuSE-tumbleweed
 docker build -t opensuse/tumbleweed:v1 docker-containers-build/docker
-docker build -t gcr.io/trihlav-1/trihlav-1-node:v4 trihlav-OpenSuSE-tumbleweed
+docker build -t gcr.io/trihlav-1/trihlav-1-node:$TRH_VER trihlav-OpenSuSE-tumbleweed
+set +e
+kubectl delete deployment,service trihlav-1-node
+#kubectl delete service trihlav-1-node
+gcloud docker push gcr.io/trihlav-1/trihlav-1-node:$TRH_VER
+kubectl run trihlav-1-node --image=gcr.io/trihlav-1/trihlav-1-node:$TRH_VER --port=32899 --expose --service-overrides='{ "spec": { "type": "LoadBalancer" } }'
+kubectl get pods
--- docker/trihlav-OpenSuSE-tumbleweed/push2gcloud.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcloud docker push gcr.io/trihlav-1/trihlav-1-node:v4
