845826ee6d5ed0a1916f3612ae0f5177cec51562
Peter Grobarcik <coder.peter.grobarcik@gmail.com>
2016-10-15 19:58:50 +0200

    Finaly a workable layout for the system user dialog.

--- src/main/cpp/trihlavSrv/trihlavWtSysUserListIView.cpp
+++ src/main/cpp/trihlavSrv/trihlavWtSysUserListIView.cpp
@@ -11,9 +11,21 @@
 #include <Wt/WTable>
 #include <Wt/WFitLayout>
 #include <Wt/WGridLayout>
+#include <Wt/WScrollArea>
+#include <Wt/WHBoxLayout>
+#include <Wt/WVBoxLayout>
+
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/attributes.hpp>
 
 #include "trihlavWtPushButton.hpp"
 #include "trihlavWtSysUserListIView.hpp"
+
+namespace {
+	static const Wt::WLength K_DLG_H(42.0, Wt::WLength::Unit::FontEm);
+	static const Wt::WLength K_DLG_W(32.0, Wt::WLength::Unit::FontEm);
+}
 
 namespace trihlav {
 
@@ -22,6 +34,11 @@ using Wt::WDialog;
 using Wt::WLength;
 using Wt::WGridLayout;
 using Wt::WFitLayout;
+using Wt::WScrollArea;
+
+using Wt::WHBoxLayout;
+using Wt::WVBoxLayout;
+
 using boost::locale::translate;
 using U = Wt::WLength::Unit;
 
@@ -33,31 +50,38 @@ WtSysUserListView::WtSysUserListView() :
 
 {
 	itsDlg->setCaption(translate("Add key").str());
-	itsDlg->setWidth(WLength(56.0, U::FontEm));
-	WGridLayout* myContentLayout = new WGridLayout;
+	itsDlg->setObjectName("WtSysUserListView");
+	itsDlg->resize(K_DLG_W, K_DLG_H);
+	WVBoxLayout* myContentLayout = new WVBoxLayout;
 	{
-		itsSysUserTable->setHeaderCount(1,Wt::Orientation::Horizontal);
-		myContentLayout->addWidget(itsSysUserTable, 0, 0, 1, 5);
+		itsSysUserTable->setObjectName("SysUserTable");
+		itsSysUserTable->setHeaderCount(1, Wt::Orientation::Horizontal);
+		myContentLayout->setStretchFactor(itsSysUserTable, 1);
+		myContentLayout->addWidget(itsSysUserTable);
 	}
+	WHBoxLayout* myBtnLayout = new WHBoxLayout;
 	{
-		myContentLayout->addWidget(itsOkBtn, 1, 1, 1, 1, Wt::AlignBottom|Wt::AlignMiddle);
-		myContentLayout->addWidget(itsCancelBtn, 1, 4, 1, 1, Wt::AlignBottom|Wt::AlignMiddle);
-		itsCancelBtn->resize(WLength(11.0, U::FontEm), WLength(2.0, U::FontEm));
-		itsOkBtn->resize(WLength(11.0, U::FontEm), WLength(2.0, U::FontEm));
+		itsCancelBtn->resize(WLength(11.0, U::FontEm), WLength(4.0, U::FontEm));
+		itsOkBtn->resize(WLength(11.0, U::FontEm), WLength(4.0, U::FontEm));
+		myBtnLayout->addWidget(itsOkBtn);
+		myBtnLayout->addWidget(itsCancelBtn);
 	}
-	myContentLayout->setRowStretch(0,1);
-	itsDlg->contents()->resize(WLength(100.0, U::Percentage), WLength(100.0, U::Percentage));
 	itsDlg->contents()->setLayout(myContentLayout);
+	itsDlg->footer()->setLayout(myBtnLayout);
+	itsDlg->setResizable(true);
 	itsDlg->finished().connect(this, &WtSysUserListView::finishedSlot);
 	itsCancelBtn->clicked().connect(itsDlg.get(), &WDialog::reject);
 	itsOkBtn->clicked().connect(itsDlg.get(), &WDialog::accept);
 }
 
 void WtSysUserListView::show(const SysUsers& pUsers) {
+	BOOST_LOG_NAMED_SCOPE("WtSysUserListView::show");
 	int myCnt = 0;
 	itsSysUserTable->clear();
-	itsSysUserTable->elementAt(0, 0)->addWidget(new Wt::WText("login"));
-	itsSysUserTable->elementAt(0, 1)->addWidget(new Wt::WText("Full name"));
+	itsSysUserTable->elementAt(0, 0)->addWidget(
+			new Wt::WText(translate("System login").str()));
+	itsSysUserTable->elementAt(0, 1)->addWidget(
+			new Wt::WText(translate("Full name").str()));
 	for (const SysUser& myUser : pUsers) {
 		myCnt++;
 		itsSysUserTable->elementAt(myCnt, 0)->addWidget(
@@ -65,7 +89,10 @@ void WtSysUserListView::show(const SysUsers& pUsers) {
 		itsSysUserTable->elementAt(myCnt, 1)->addWidget(
 				new Wt::WText(myUser.itsFullName));
 	}
-	itsDlg->exec();
+	BOOST_LOG_TRIVIAL(debug)<<"System users loaded.";
+	itsDlg->setModal(true);
+	itsDlg->resize(K_DLG_W, K_DLG_H);
+	itsDlg->show();
 }
 
 void WtSysUserListView::finishedSlot(WDialog::DialogCode pCode) {
